6f73677c028f58189542537a6791ea91cd88b60b
Matthew Riley <matthewriley@Interapts-MacBook-Pro-10.local>
2018-10-10 23:35:32 -0500

    Initial Commit

---
+++ TriviaGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriviaGame
+//
+//  Created by Matthew Riley on 10/10/18.
+//  Copyright © 2018 Matthew Riley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
